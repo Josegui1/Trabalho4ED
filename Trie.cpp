c739aabbf3a8295550cd6fa8d18bb944d193fd4f
Josegui1 <antunesjguilherme@gmail.com>
2026-05-26 14:31:11 -0300

    Fazendo a função toSearchKey

--- Trie.cpp
+++ Trie.cpp
@@ -1,2 +1,20 @@
 #include "Trie.hpp"
 
+std::string Trie::toSearchKey(std::string text){
+    std::string key = "";
+    for (int i = 0; i<text.length(); i++){
+        char c  = text[i];
+        
+        if(c == ' '){
+            continue;
+        }
+
+        if (c >= 'A' && c <= 'Z'){
+            c = c - 'A' + 'a';
+        }
+
+        key += c;
+    }
+
+    return key;
+}
