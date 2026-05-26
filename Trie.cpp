82b9edc18b9b5f51ebcf0bbf241e0b4e2ef62acc
unknown <brunocavalli12b@gmail.com>
2026-05-26 14:37:26 -0300

    add construtor e destrutor

--- Trie.cpp
+++ Trie.cpp
@@ -1,5 +1,37 @@
 #include "Trie.hpp"
 
+TrieNode::TrieNode(){
+    isEndOfTitle = false;
+    game = nullptr;
+
+    for (int i = 0; i < ALPHABET_SIZE; i++ ){
+        children[i] = nullptr;
+    }
+
+
+}
+
+
+TrieNode::~TrieNode(){
+
+    for(int i = 0; i < ALPHABET_SIZE; i++){
+        if(children[i] != nullptr){
+            delete children[i];
+        }
+    }
+}
+
+Trie::Trie(){
+    root = new TrieNode;
+}
+
+Trie::~Trie(){
+    if(root != nullptr){
+        delete root;
+    }
+}
+
+
 std::string Trie::toSearchKey(std::string text){
     std::string key = "";
     for (int i = 0; i<text.length(); i++){
