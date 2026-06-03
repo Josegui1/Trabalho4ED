756e44f1474ea6f37db63a7d1ce5caa5552ee981
BrunoCavalli <brunocavalli12b@gmail.com>
2026-06-03 19:03:19 -0300

    criei main inicial

---
+++ main.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <vector>
+#include <string>
+
+#include "Game.hpp"
+#include "Trie.hpp"
+#include "GameDatabase.cpp"
+
+int main(argc, char*, argv[]){
+  if (argc != 3){
+    std::cout << "./app k prefixo" << std::endl;
+    return 1;
+  }
+  
+
+  int k = std::stoi(argv[1]);
+  std::string prefixo = argv[2];
+
+  Trie trie;
+
+  for (int i = 0; i < numberOfGames; i++){
+    trie.insert(&games[i]);
+  }
+
+  std::vector<Game*> results = trie.autocomplete(prefix, k)
+
+
+  if (results.empty()) {
+        std::cout << "No results found" << std::endl;
+        return 0;
+    }
+
+
+  for (int i = 0; i < results.size(); i++) {
+        std::cout 
+            << results[i]->getTitle() << " | "
+            << results[i]->getShortDescription() << " | "
+            << results[i]->getPopularity()
+            << std::endl;
+    }
+
+
+  return 0;
+  
+}
